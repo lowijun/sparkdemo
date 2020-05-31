92a62dbbd7463b138f714db8bfc558493aff2d2b
lowi <2416268001@qq.com>
2020-05-31 09:12:49 +0800

    sparkRDD action算子练习

--- src/main/scala/com/lowi/rddDemo/action/FirstTest.scala
+++ src/main/scala/com/lowi/rddDemo/action/FirstTest.scala
@@ -1,5 +1,27 @@
 package com.lowi.rddDemo.action
 
-object FirstTest {
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
 
+object FirstTest {
+  def main(args: Array[String]): Unit = {
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[2]").setAppName("FirstTest")
+    val sc = new SparkContext(sparkConf)
+    sc.setLogLevel("warn")
+    val rdd1: RDD[(String, String)] = sc.makeRDD(Array(("A","1"),("B","2"),("C","3")),2)
+    val tuple: (String, String) = rdd1.first()
+    println(tuple)
+
+    /**
+      * 结果
+      * (String, String) = (A,1)
+      */
+    val rdd2: RDD[Int] = sc.makeRDD(Seq(10, 4, 2, 12, 3))
+    val i: Int = rdd2.first()
+    println(i)
+    /**
+      * Int = 10
+      */
+    sc.stop()
+  }
 }
